0c58578fea45479c3de49bd121b3892a997a49cd
thinkaboutryry <ryder.barrios@uac.edu.co>
2026-03-14 14:12:59 -0500

    feat: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
+  import { SpeedInsights } from "@vercel/speed-insights/react";
   import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <>
+      <App />
+      <SpeedInsights />
+    </>
+  );
   
